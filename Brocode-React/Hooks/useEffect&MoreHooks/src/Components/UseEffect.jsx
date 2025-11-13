e8517474d4411b3cd5d8b753007d39ac07c28212
creative-atish <atishkumar6115@gmail.com>
2025-11-13 11:15:55 +0530

    renamed

--- Brocode-React/Hooks/useEffect&MoreHooks/src/Components/UseEffect.jsx
+++ Brocode-React/Hooks/useEffect&MoreHooks/src/Components/UseEffect.jsx
@@ -1,3 +1,22 @@
+// useEffect : 
+// React hook ek function hai jo component render hone ke baad side effects
+// (jaise data fetch, DOM update, event listener, etc.) handle karta hai.
+
+//3 TYPES OF SYNTAX : 
+
+//useEffect(() => {
+  // code runs after every render
+//});
+
+// useEffect(() => {
+//   // code runs only once
+// }, []);
+
+// useEffect(() => {
+//   // code runs when 'count' changes
+// }, [count]);
+
+
 import React, { useEffect, useState } from 'react';
 
 const UseEffect = () => {
