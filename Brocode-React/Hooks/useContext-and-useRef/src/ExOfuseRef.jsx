99e64099fc96ddcf8108c24071a7390afd0a736b
creative-atish <atishkumar6115@gmail.com>
2025-11-21 21:53:00 +0530

    useRef completed

--- Brocode-React/Hooks/useContext-and-useRef/src/ExOfuseRef.jsx
+++ Brocode-React/Hooks/useContext-and-useRef/src/ExOfuseRef.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+// import React, { useState } from "react";
 
-const ExOfuseRef = () => {
+// const CounterUsingState = () => {
+//   const [count, setCount] = useState(0);
+
+//   const handleClick = () => {
+//     setCount(prev => prev + 1);
+//     console.log("useState Count:", count + 1); 
+//   };
+
+//   return (
+//     <>
+//       <h2>useState Counter</h2>
+//       <p>UI Count: {count}</p>
+//       <button onClick={handleClick}>Increase</button>
+//     </>
+//   );
+// };
+
+// export default CounterUsingState;
+
+
+
+// now example of useRef() -------------------------------------------------
+
+import React, { useRef } from "react";
+
+const CounterUsingRef = () => {
+  const countRef = useRef(0);
+
+  const handleClick = () => {
+    countRef.current = countRef.current + 1;
+    console.log("useRef Count:", countRef.current);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <h2>useRef Counter</h2>
+      <p>UI Count: (won't change)</p>
+      <button onClick={handleClick}>Increase</button>
+    </>
+  );
+};
 
-export default ExOfuseRef
+export default CounterUsingRef;
